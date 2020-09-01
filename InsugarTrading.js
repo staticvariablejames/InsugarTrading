6f04d940cac9bf91487d6d11470091c774028437
Static Variable <staticvariablejames@gmail.com>
2020-08-31 22:09:55 -0500

    InsugarTrading.js: Rename InsugarTrading.values to InsugarTrading.data.

--- InsugarTrading.js
+++ InsugarTrading.js
@@ -7,22 +7,31 @@ InsugarTrading.minigame = null; // Set to Game.Objects['Bank'].minigame on Insug
 InsugarTrading.isGatheringData = false;
 InsugarTrading.tickCount = 0;
 
+/* InsugarTrading.data is initialized by InsugarTrading.startDataCollection().
+ * The data here is just a histogram:
+ * data[i][j] is the number of times that the i-th stock
+ * (as listed by Game.Objects['Bank'].goodsById)
+ * had its price between j/10 (inclusive) and j/10+1 (exclusive).
+ * So this histogram goes in 10-cents increments.
+ */
+InsugarTrading.data = null;
+
 InsugarTrading.customTick = function() {
     if(!InsugarTrading.isGatheringData) return;
-    for(let i = 0; i < InsugarTrading.values.length; i++) {
+    for(let i = 0; i < InsugarTrading.data.length; i++) {
         let value = InsugarTrading.minigame.goodsById[i].val;
         value = Math.floor(10*value);
-        InsugarTrading.values[i][value]++;
+        InsugarTrading.data[i][value]++;
     }
     InsugarTrading.tickCount++;
 }
 
 InsugarTrading.startDataCollection = function() {
-    InsugarTrading.values = new Array(InsugarTrading.minigame.goodsById.length);
-    for(let i = 0; i < InsugarTrading.values.length; i++) {
-        InsugarTrading.values[i] = new Array(3000);
-        for(let j = 0; j < InsugarTrading.values[i].length; j++)
-            InsugarTrading.values[i][j] = 0;
+    InsugarTrading.data = new Array(InsugarTrading.minigame.goodsById.length);
+    for(let i = 0; i < InsugarTrading.data.length; i++) {
+        InsugarTrading.data[i] = new Array(3000);
+        for(let j = 0; j < InsugarTrading.data[i].length; j++)
+            InsugarTrading.data[i][j] = 0;
     }
 
     InsugarTrading.tickCount = 0;
