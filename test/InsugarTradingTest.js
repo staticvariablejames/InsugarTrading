cf29410b878eacc585e9ddcd3c05c09697c06f70
Static Variable <staticvariablejames@gmail.com>
2020-09-08 22:57:19 -0500

    Refactor code to handle different bank levels.

--- test/InsugarTradingTest.js
+++ test/InsugarTradingTest.js
@@ -7,8 +7,8 @@ let almostEqual = function(a, b) {
 }
 
 // Testing dataset
-InsugarTrading.tickCount = 20;
-InsugarTrading.data = [
+InsugarTrading.tickCount = [null, 20];
+InsugarTrading.data = [null, [
     [ 0, 1, 2, 3, 4, 4, 3, 2, 1, 0, 0, 0],
     [ 0, 1, 2, 3, 4, 4, 3, 2, 1, 0, 0, 0],
     [ 0, 1, 2, 3, 4, 4, 3, 2, 1, 0, 0, 0],
@@ -25,47 +25,56 @@ InsugarTrading.data = [
     [ 0, 1, 2, 3, 4, 4, 3, 2, 1, 0, 0, 0],
     [ 0, 1, 2, 3, 4, 4, 3, 2, 1, 0, 0, 0],
     [ 0, 1, 2, 3, 4, 4, 3, 2, 1, 0, 0, 0],
-];
+]];
 
-console.assert(InsugarTrading.data.length === 16); // Testing the test
-console.assert(InsugarTrading.data[0].length === 12); // Testing the test
+// Clear the cache
+InsugarTrading.partialSums = null;
+
+console.assert(InsugarTrading.data[1].length === 16); // Testing the test
+console.assert(InsugarTrading.data[1][0].length === 12); // Testing the test
 // The extra row is to test independence from Game.Objects.Bank.minigame.objectsById.length
+
+// Pretend the bank has level 1
+InsugarTrading.getBankLevel = () => 1;
+// Pretend there are 16 goods
+InsugarTrading.getGoodsCount = () => 16;
 
 InsugarTrading.computePartialSums();
 
 console.assert(InsugarTrading.partialSums !== null);
-console.assert(InsugarTrading.partialSums.length === 16);
-console.assert(InsugarTrading.partialSums[0].length === 13);
-console.assert(InsugarTrading.partialSums[0].join(',') === "0,0,1,3,6,10,14,17,19,20,20,20,20");
+console.assert(InsugarTrading.partialSums.length === 2);
+console.assert(InsugarTrading.partialSums[1].length === 16);
+console.assert(InsugarTrading.partialSums[1][0].length === 13);
+console.assert(InsugarTrading.partialSums[1][0].join(',') === "0,0,1,3,6,10,14,17,19,20,20,20,20");
 
-console.assert(InsugarTrading.quantile(0, -1) === -Infinity);
-console.assert(InsugarTrading.quantile(0, 1.01) === Infinity);
-console.assert(almostEqual(InsugarTrading.quantile(0, 0), 0.1));
-console.assert(almostEqual(InsugarTrading.quantile(0, 0.025), 0.15));
-console.assert(almostEqual(InsugarTrading.quantile(0, 0.0375), 0.175));
-console.assert(almostEqual(InsugarTrading.quantile(0, 0.05), 0.2));
-console.assert(almostEqual(InsugarTrading.quantile(0, 0.10), 0.25));
-console.assert(almostEqual(InsugarTrading.quantile(0, 0.15), 0.3));
-console.assert(almostEqual(InsugarTrading.quantile(0, 0.3), 0.4));
-console.assert(almostEqual(InsugarTrading.quantile(0, 0.5), 0.5));
-console.assert(almostEqual(InsugarTrading.quantile(0, 0.6), 0.55));
-console.assert(almostEqual(InsugarTrading.quantile(0, 0.7), 0.6));
-console.assert(almostEqual(InsugarTrading.quantile(0, 1), 1.2));
+console.assert(InsugarTrading.quantile(1, 0, -1) === -Infinity);
+console.assert(InsugarTrading.quantile(1, 0, 1.01) === Infinity);
+console.assert(almostEqual(InsugarTrading.quantile(1, 0, 0), 0.1));
+console.assert(almostEqual(InsugarTrading.quantile(1, 0, 0.025), 0.15));
+console.assert(almostEqual(InsugarTrading.quantile(1, 0, 0.0375), 0.175));
+console.assert(almostEqual(InsugarTrading.quantile(1, 0, 0.05), 0.2));
+console.assert(almostEqual(InsugarTrading.quantile(1, 0, 0.10), 0.25));
+console.assert(almostEqual(InsugarTrading.quantile(1, 0, 0.15), 0.3));
+console.assert(almostEqual(InsugarTrading.quantile(1, 0, 0.3), 0.4));
+console.assert(almostEqual(InsugarTrading.quantile(1, 0, 0.5), 0.5));
+console.assert(almostEqual(InsugarTrading.quantile(1, 0, 0.6), 0.55));
+console.assert(almostEqual(InsugarTrading.quantile(1, 0, 0.7), 0.6));
+console.assert(almostEqual(InsugarTrading.quantile(1, 0, 1), 1.2));
 
-console.assert(InsugarTrading.inverseQuantile(0, -1) === 0);
-console.assert(InsugarTrading.inverseQuantile(0, 0) === 0);
-console.assert(InsugarTrading.inverseQuantile(0, 1.21) === 1);
-console.assert(almostEqual(InsugarTrading.inverseQuantile(0, 0.1), 0));
-console.assert(almostEqual(InsugarTrading.inverseQuantile(0, 0.15), 0.025));
-console.assert(almostEqual(InsugarTrading.inverseQuantile(0, 0.175), 0.0375));
-console.assert(almostEqual(InsugarTrading.inverseQuantile(0, 0.2), 0.05));
-console.assert(almostEqual(InsugarTrading.inverseQuantile(0, 0.25), 0.10));
-console.assert(almostEqual(InsugarTrading.inverseQuantile(0, 0.3), 0.15));
-console.assert(almostEqual(InsugarTrading.inverseQuantile(0, 0.4), 0.3));
-console.assert(almostEqual(InsugarTrading.inverseQuantile(0, 0.5), 0.5));
-console.assert(almostEqual(InsugarTrading.inverseQuantile(0, 0.55), 0.6));
-console.assert(almostEqual(InsugarTrading.inverseQuantile(0, 0.6), 0.7));
-console.assert(almostEqual(InsugarTrading.inverseQuantile(0, 1.0), 1));
-console.assert(almostEqual(InsugarTrading.inverseQuantile(0, 1.2), 1));
+console.assert(InsugarTrading.inverseQuantile(1, 0, -1) === 0);
+console.assert(InsugarTrading.inverseQuantile(1, 0, 0) === 0);
+console.assert(InsugarTrading.inverseQuantile(1, 0, 1.21) === 1);
+console.assert(almostEqual(InsugarTrading.inverseQuantile(1, 0, 0.1), 0));
+console.assert(almostEqual(InsugarTrading.inverseQuantile(1, 0, 0.15), 0.025));
+console.assert(almostEqual(InsugarTrading.inverseQuantile(1, 0, 0.175), 0.0375));
+console.assert(almostEqual(InsugarTrading.inverseQuantile(1, 0, 0.2), 0.05));
+console.assert(almostEqual(InsugarTrading.inverseQuantile(1, 0, 0.25), 0.10));
+console.assert(almostEqual(InsugarTrading.inverseQuantile(1, 0, 0.3), 0.15));
+console.assert(almostEqual(InsugarTrading.inverseQuantile(1, 0, 0.4), 0.3));
+console.assert(almostEqual(InsugarTrading.inverseQuantile(1, 0, 0.5), 0.5));
+console.assert(almostEqual(InsugarTrading.inverseQuantile(1, 0, 0.55), 0.6));
+console.assert(almostEqual(InsugarTrading.inverseQuantile(1, 0, 0.6), 0.7));
+console.assert(almostEqual(InsugarTrading.inverseQuantile(1, 0, 1.0), 1));
+console.assert(almostEqual(InsugarTrading.inverseQuantile(1, 0, 1.2), 1));
 
 console.log("Testing done!");
