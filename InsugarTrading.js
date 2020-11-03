878ee9ea0b7da8e8a90309812e2f3d53e1c3d682
Static Variable James <staticvariablejames@gmail.com>
2020-11-02 21:28:35 -0600

    Replace "var" with "let" in the initialization
    "var" was previously there to allow the script to be loaded repeatedly.
    Since we are using the v2.031 modding API,
    reloading the script is not possible anyway
    (because Game.registerMod rejects duplicates),
    so we might as well uniformize everything to "let".

--- InsugarTrading.js
+++ InsugarTrading.js
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-var InsugarTrading = {};
-// 'var' used here to avoid syntax errors if this script is loaded more than once
+let InsugarTrading = {};
 if(typeof CCSE == 'undefined') Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js');
 // CCSE calls Game.Win('Third-party') for us
 
