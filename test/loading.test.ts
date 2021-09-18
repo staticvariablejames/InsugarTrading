ef5130c363238b0b40d7ac6d1d0b543b7630d560
Static Variable James <staticvariablejames@gmail.com>
2021-09-18 11:23:24 -0500

    loading.test.ts: Use test.beforeEach.

--- test/loading.test.ts
+++ test/loading.test.ts
@@ -1,26 +1,23 @@
 import { test, expect } from '@playwright/test';
 import { setupCookieClickerPage } from 'cookie-connoisseur';
 
-test('The mod loads itself', async ({page}) => {
+test.beforeEach(async ({page}) => {
     page = await setupCookieClickerPage(page);
     await page.evaluate(() => Game.LoadMod('https://staticvariablejames.github.io/InsugarTrading/InsugarTrading.js'));
     await page.waitForFunction(() => 'Insugar Trading' in Game.mods);
+});
+
+test('The mod loads itself', async ({page}) => {
     expect(await page.evaluate('InsugarTrading')).not.toBeFalsy();
 });
 
 test('The mod loads its data', async ({page}) => {
-    page = await setupCookieClickerPage(page);
-    await page.evaluate(() => Game.LoadMod('https://staticvariablejames.github.io/InsugarTrading/InsugarTrading.js'));
-    await page.waitForFunction(() => 'Insugar Trading' in Game.mods);
     await page.evaluate('InsugarTrading.fetchDataset(5)');
     await page.waitForFunction('InsugarTrading.data[5].length != 0');
     expect(await page.evaluate('InsugarTrading.data[5].length')).toEqual(16);
 });
 
 test('Functions in onDatasetLoad are properly called', async ({page}) => {
-    page = await setupCookieClickerPage(page);
-    await page.evaluate(() => Game.LoadMod('https://staticvariablejames.github.io/InsugarTrading/InsugarTrading.js'));
-    await page.waitForFunction(() => 'Insugar Trading' in Game.mods);
     await page.evaluate('window.calledWith = null');
     await page.evaluate('InsugarTrading.onDatasetLoad.push(n => {window.calledWith = n})');
     await page.evaluate('InsugarTrading.fetchDataset(5)');
